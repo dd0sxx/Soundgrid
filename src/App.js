8216d95c0e67de798da39236eca3a6fb5fcb7e15
dd0sxx <contacttheotelonis@gmail.com>
2021-02-11 11:11:30 -0500

    added logo and fixed styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
 import mesh from './mesh';
 import {useEffect, useState} from 'react';
+import logo from './noware_website_logo.gif';
 
 function App() {
   const [effect, setEffect] = useState(null);
@@ -13,10 +14,11 @@ function App() {
   return (
     <div className="App">
       <header>
-      <h1>Enable your microphone and Click Around! the audio is used reactively with the graphics ^__^ </h1>
-      <h3> If you see nothing after clicking on circles or mesh, try refreshing. Sometimes the audio bins don't load properly </h3>
-      <button id='mesh' onClick={changeEffect}>mesh</button>
-      <button id='circles' onClick={changeEffect}>circles</button>
+      <img src={logo} unselectable="on"/>
+      <h1 unselectable="on">Enable your microphone and Click Around! the audio is used reactively with the graphics ^__^ </h1>
+      <h3 unselectable="on"> If you see nothing after clicking on circles or mesh, try refreshing. Sometimes the audio bins don't load properly </h3>
+      <button id='mesh' onClick={changeEffect} unselectable="on">mesh</button> 
+      <button id='circles' onClick={changeEffect} unselectable="on">circles</button>
       </header>
       <div className='background-div'>
         {effect === 'mesh'  ? 
